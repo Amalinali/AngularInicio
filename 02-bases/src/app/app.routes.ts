3eddc73669be32e1680ea306381c0947899062bd
Denisse Jimenez <nali.jimenez14@gmail.com>
2025-08-27 17:00:40 -0600

    Inicio de bases Angular

---
+++ 02-bases/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { CounterPageComponent } from './pages/counter-page.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [{
+  path: '',
+  component: CounterPageComponent,
+}
+
+
+
+
+];
