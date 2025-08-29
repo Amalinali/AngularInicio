5146f785b58158a9e1ecf8efa5637c3b939a26c6
Denisse Jimenez <nali.jimenez14@gmail.com>
2025-08-29 14:06:49 -0600

    Angular base terminada

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,20 +1,27 @@
+import { UpperCasePipe } from "@angular/common";
 import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: './hero-page.component.html'
+  templateUrl: './hero-page.component.html',
+  imports: [ UpperCasePipe],
 })
 
 export class HeroPageComponent {
+  //señales, forma actualizada de cambiar el estado de la aplicación
   name = signal('Ironman');
   age = signal(45);
 
+  //señal computada, SON SEÑALES DE SOLO LECTURA 
   heroDescription = computed(() => {
-      const description = `${ this.name()} - ${this.age()}`;
+      const description = `${ this.name() } - ${ this.age() }`;
       return description;
-  })
+  });
+
+  //
+  capitalizedName = computed( () => this.name().toUpperCase());
 
   getHeroDescription(){
-    return `${this.name()} - ${this.age}`;
+    return `${this.name()} - ${this.age()}`;
   }
 
   changeHero(){
