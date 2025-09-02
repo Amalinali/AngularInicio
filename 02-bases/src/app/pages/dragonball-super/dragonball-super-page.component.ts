19d017cf1118432d1cfd575a76342fb68da751c4
Denisse Jimenez <nali.jimenez14@gmail.com>
2025-09-02 16:51:29 -0600

    avances angular bases

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -10,29 +10,21 @@ interface Character {
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
-  templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css'
+  templateUrl: './dragonball-super-page.component.html',
 })
 
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
 
 
-  name = signal('Gohan');
-  power = signal(100);
+  name = signal('');
+  power = signal(0);
 
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Picolo', power: 3000 },
-    { id: 3, name: 'Yamcha', power: 500 },
   ]);
 
-  powerClasses = computed(() => {
-    return {
-      'text-danger': true,
-    };
-  });
 
   addCharacter() {
     if( !this.name() || !this.power() || this.power() <= 0 ){
